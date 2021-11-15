63c7c70e60e1649959af2366232b2409de7fb4cf
Mogakamo <mogaka.amo254@gmail.com>
2021-11-15 12:19:51 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import axios from 'axios'
 
 function App() {
-  const [user, setUsers] = useState([])
+  const [users, setUsers] = useState([])
   const [text, setText] = useState('')
+  const [suggestions, setSuggestions] = useState([])
   useEffect(() => {
     const loadUsers = async() => {
       const response = await axios.get('https://reqres.in/api/users')
@@ -16,6 +17,16 @@ function App() {
   }, [])
 
   const onChangeHandler = (text) => {
+    let matches = []
+    if(text.length > 0) {
+      matches = users.filter(user=> {
+        const regex = new RegExp(`${text}`, "gi")
+        return user.email.match(regex)
+      })
+    }
+
+    console.log('matches', matches)
+    setSuggestions(matches)
     setText(text)
   }
 
