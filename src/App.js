4dd61888cc5d1a4ce8527f7bfb4b8dd802b3a181
Mogakamo <mogaka.amo254@gmail.com>
2021-11-15 12:06:01 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [user, setUsers] = useState([])
+  const [text, setText] = useState('')
+  useEffect(() => {
+    const loadUsers = async() => {
+      const response = await axios.get('https://reqres.in/api/users')
+      console.log(response)
+      setUsers(response.data.data)
+    }
+
+    loadUsers()
+  }, [])
+
+  const onChangeHandler = (text) => {
+    setText(text)
+  }
+
   return (
     <div className="App">
-      <input type="text" />
+      
+      <input type="text"
+            className="col-md-6 input"
+            style={{marginTop: 10}} 
+            onChange={e=>onChangeHandler(e.target.value)}
+      />
     </div>
   );
 }
